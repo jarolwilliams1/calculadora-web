6124956a95ee46ef1a5461ec222c7d9120bf2cc9
jarolwilliams1 <215430519+jarolwilliams1@users.noreply.github.com>
2025-12-19 01:39:45 -0400

    agregar funcionalidad

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,21 @@
-const cambio = document.getElementById("btnula");
-let cambiado;
-const fecha = new Date(2007, 11, 8)
-alert(fecha)
-if (cambio.click)
-    {
-    alert("hola, presionate un boton");
-}
-if (!cambio){
+
+const lista = [2,5,7,9,7,5,3,5,7,8,3,1,12,4,9,0,6,3,];
+
+
+ async function Numeros(lista)
+  {
+
+    for (let i = 0; i < lista.length; i++){
+
+    if (lista[i] % 2 == 0){
+    alert(lista[i]);
+    }
+   
 
 }
+    
+}
+
+Numeros(lista)
+
+
