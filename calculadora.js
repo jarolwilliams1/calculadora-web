16001cf56d73e48fc8211e533ea2f1df75ef1e5c
jarolwilliams1 <215430519+jarolwilliams1@users.noreply.github.com>
2025-12-19 01:04:45 -0400

    Subiendo proyecto inicial

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+const cambio = document.getElementById("btnula");
+let cambiado;
+const fecha = new Date(2007, 11, 8)
+alert(fecha)
+if (cambio.click)
+    {
+    alert("hola, presionate un boton");
+}
+if (!cambio){
+
+}
